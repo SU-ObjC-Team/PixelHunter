f48d34d136fbf1640c38e52eb83b75b8c8b52e0c
Rostyslav Druzhchenko <dr.rost.dev@gmail.com>
2014-01-25 12:45:11 +0200

    Code review fixes - Naming and formatting improved in SUPixelHunter class.

--- PixelHunter/Classes/SUPixelHunter.m
+++ PixelHunter/Classes/SUPixelHunter.m
@@ -13,11 +13,12 @@
 #import <CoreMotion/CoreMotion.h>
 
 
-static CGFloat const kSUAccelerationThreshold = 1.7f;
-static CGFloat const kSUAccelerometerUpdateInterval = 0.1f;
+static const CGFloat kSUAccelerationThreshold = 1.7f;
+static const CGFloat kSUAccelerometerUpdateInterval = 0.1f;
 static const NSInteger kSUAlertButtonIndexCancel = 0;
 
-@interface SUPixelHunter () <UIAccelerometerDelegate, SUGridViewControllerDelegate>
+@interface SUPixelHunter () <UIAccelerometerDelegate,
+                             SUGridViewControllerDelegate>
 
 @property (nonatomic, weak) UIAlertView *alertView;
 @property (nonatomic, strong) UIWindow *debugWindow;
@@ -39,17 +40,7 @@ static id __sharedInstance;
     dispatch_once(&onceToken, ^{
 
         __sharedInstance = [[SUPixelHunter alloc] init];
-
-        NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
-        [notificationCenter addObserver:__sharedInstance
-                               selector:@selector(orientationChanged:)
-                                   name:UIApplicationDidChangeStatusBarOrientationNotification
-                                 object:nil];
-
-        [notificationCenter addObserver:__sharedInstance
-                               selector:@selector(removeWindowForDebug)
-                                   name:UIApplicationDidEnterBackgroundNotification
-                                 object:nil];
+        [__sharedInstance subscribeForSystemNotifications];
     });
 
     return __sharedInstance;
@@ -59,6 +50,7 @@ static id __sharedInstance;
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
+
         __sharedInstance = nil;
         __sharedInstance = [super allocWithZone:zone];
     });
@@ -74,20 +66,39 @@ static id __sharedInstance;
 - (id)init
 {
     if (self = [super init]) {
+
         self.motionManager = [[CMMotionManager alloc] init];
         self.motionManager.accelerometerUpdateInterval = kSUAccelerometerUpdateInterval;
         
-        [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue]
-                                                 withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
-                                                     if (error) {
-                                                         [self.motionManager stopAccelerometerUpdates];
-                                                     } else {
-                                                         [self performSelectorOnMainThread:@selector(handleShake:) withObject:accelerometerData waitUntilDone:YES];
-                                                     }
-                                                 }];
+        NSOperationQueue *currentQueue = [NSOperationQueue currentQueue];
+        [self.motionManager startAccelerometerUpdatesToQueue:currentQueue
+         withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
+             
+             if (error) {
+                 [self.motionManager stopAccelerometerUpdates];
+             } else {
+                 [self performSelectorOnMainThread:@selector(handleShake:)
+                                        withObject:accelerometerData waitUntilDone:YES];
+             }
+         }];
     }
     
     return self;
+}
+
+- (void)subscribeForSystemNotifications
+{
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    
+    [notificationCenter addObserver:self
+                           selector:@selector(orientationChanged:)
+                               name:UIApplicationDidChangeStatusBarOrientationNotification
+                             object:nil];
+    
+    [notificationCenter addObserver:self
+                           selector:@selector(removeWindowForDebug)
+                               name:UIApplicationDidEnterBackgroundNotification
+                             object:nil];
 }
 
 #pragma mark - Public methods
@@ -102,24 +113,42 @@ static id __sharedInstance;
 - (void)handleShake:(CMAccelerometerData *)accelerometerData
 {
     CMAcceleration acceleration = accelerometerData.acceleration;
-    if (acceleration.x > kSUAccelerationThreshold
-        || acceleration.y > kSUAccelerationThreshold
-        || acceleration.z > kSUAccelerationThreshold) {
+    if ([self shouldDebugModeBeInit:acceleration]) {
+
+        [self activateZGestureView];
+    }
+}
+
+- (BOOL)shouldDebugModeBeInit:(CMAcceleration)acceleration
+{
+    BOOL result = NO;
+
+    if (acceleration.x > kSUAccelerationThreshold ||
+        acceleration.y > kSUAccelerationThreshold ||
+        acceleration.z > kSUAccelerationThreshold) {
+        
         if (self.alertView == nil && self.debugWindow == nil) {
-            [self createZGestureView];
+            result = YES;
         }
     }
+
+    return result;
 }
 
 - (void)showAlert
 {
+    NSString *aletTitle = NSLocalizedStringFromTable(@"ENTER_UI_DEBUG_MODE",
+                                                     @"PixelHunter", @"Enter UI debug mode");
+    NSString *cancelTitle = NSLocalizedStringFromTable(@"CANCEL", @"PixelHunter", @"Cancel");
+    NSString *enterTitle = NSLocalizedStringFromTable(@"ENTER", @"PixelHunter", @"Enter");
+
+    // Create a local variable because alertView property is weak
     UIAlertView *alertView = [[UIAlertView alloc]
-                              initWithTitle:NSLocalizedStringFromTable(@"ENTER_UI_DEBUG_MODE", @"PixelHunter", @"Enter UI debug mode")
+                              initWithTitle:aletTitle
                               message:nil
                               delegate:self
-                              cancelButtonTitle:NSLocalizedStringFromTable(@"CANCEL", @"PixelHunter", @"Cancel")
-                              otherButtonTitles:NSLocalizedStringFromTable(@"ENTER", @"PixelHunter", @"Enter"),
-                              nil];
+                              cancelButtonTitle:cancelTitle
+                              otherButtonTitles:enterTitle, nil];
     [alertView show];
     self.alertView = alertView;
 }
@@ -132,22 +161,27 @@ static id __sharedInstance;
     }
 }
 
-- (void)createZGestureView
+- (void)activateZGestureView
 {
-    UIWindow *window = [[[UIApplication sharedApplication] windows] objectAtIndex:0];
-    SUZGestureView *zGestureView = [[SUZGestureView alloc] initWithFrame:window.rootViewController.view.frame];
+    UIWindow *topWindow = [self getTopWindow];
+    CGRect rc = topWindow.rootViewController.view.frame;
+    SUZGestureView *zGestureView = [[SUZGestureView alloc] initWithFrame:rc];
     [zGestureView.zGestureRecognizer addTarget:self action:@selector(showAlert)];
-    [window.rootViewController.view addSubview:zGestureView];
+    [topWindow.rootViewController.view addSubview:zGestureView];
 }
 
 - (void)showWindowForDebug
 {
-    self.parentWindow = [[[UIApplication sharedApplication] windows] objectAtIndex:0];
-    self.debugWindow = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    UIImage *debugWindowImage = [SUPixelHunterScreenshotUtil convertViewToImage:
-                      [[[[[UIApplication sharedApplication] windows] objectAtIndex:0] rootViewController] view]];
-    SUGridViewController *viewController = [[SUGridViewController alloc] initWithScreenshotImage:debugWindowImage];
+    self.parentWindow = [self getTopWindow];
+    UIView *view = [[self.parentWindow rootViewController] view];
+    UIImage *debugWindowImage = [SUPixelHunterScreenshotUtil convertViewToImage:view];
+
+    SUGridViewController *viewController =
+        [[SUGridViewController alloc] initWithScreenshotImage:debugWindowImage];
     viewController.delegate = self;
+
+    CGRect rc = [self.parentWindow bounds];
+    self.debugWindow = [[UIWindow alloc] initWithFrame:rc];
     self.debugWindow.rootViewController = viewController;
     self.parentWindow.hidden = YES;
     [self.debugWindow makeKeyAndVisible];
@@ -160,12 +194,20 @@ static id __sharedInstance;
     [self.alertView dismissWithClickedButtonIndex:0 animated:NO];
 }
 
+- (UIWindow *)getTopWindow
+{
+    return [[[UIApplication sharedApplication] windows] objectAtIndex:0];
+}
+
 #pragma mark - Orientation handling
+
 - (void)orientationChanged:(NSNotification *)notification
 {
     [self.debugWindow.rootViewController.view setNeedsLayout];
     [((SUGridViewController *)self.debugWindow.rootViewController).gridRootView layoutViewsDependingOnOrientation];
 }
+
+#pragma mark - SUGridViewControllerDelegate
 
 - (void)tapOnCloseButton
 {
